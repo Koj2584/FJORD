ff606ada17030a6203c22aaf87913f8ce13b6a4f
Koj2584 <vomelaj.05@spst.eu>
2022-02-25 21:41:13 +0100

    Oprava

---
+++ Gym Jump/Assets/Scripts/Death.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Death : MonoBehaviour
+{
+    public GameObject player;
+    public Transform camera;
+
+    bool jednou = true;
+
+    void Update()
+    {
+        if(player.transform.position.y <= gameObject.transform.position.y && jednou)
+        {
+            jednou = false;
+
+            // Co se stane po smrti
+            Debug.Log("Die");
+            player.SetActive(false);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
---
+++ Gym Jump/Assets/Scripts/JumpPlatform.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpPlatform : MonoBehaviour
+{
+    public float jumpHeight = 20f;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.relativeVelocity.y <= 0f)
+        {
+            Rigidbody2D rg = collision.collider.GetComponent<Rigidbody2D>();
+            if (rg != null)
+            {
+                rg.velocity = new Vector2(rg.velocity.x, jumpHeight);
+            }
+        }
+    }
+}
