b1e7ea2b726665be590c5bd1973fa9736d2e2455
Suc-h <97514706+Suc-h@users.noreply.github.com>
2022-02-26 10:04:58 +0100

    Uh oprava chyby

--- Gym Jump/Assets/Scripts/PauseMenu.cs
+++ Gym Jump/Assets/Scripts/PauseMenu.cs
@@ -9,7 +9,8 @@ public class PauseMenu : MonoBehaviour
    
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Escape))
+       
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
             if(GameIsPaused)
             {
@@ -27,18 +28,20 @@ public class PauseMenu : MonoBehaviour
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
         GameIsPaused = false;
-
+       
     }
     void Pause()
     {
+        
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         GameIsPaused = true;
+       
 
     }
     public void LoadMenu()
     {
-
+        Time.timeScale = 1f;
         SceneManager.LoadScene("Menu");
 
     }
