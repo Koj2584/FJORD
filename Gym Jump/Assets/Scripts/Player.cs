adf9aeae1ff7aba18a7ee6d2fbe2a7a79a2cf786
Koj2584 <vomelaj.05@spst.eu>
2022-03-03 21:53:15 +0100

    Práce na itemech

--- Gym Jump/Assets/Scripts/Player.cs
+++ Gym Jump/Assets/Scripts/Player.cs
@@ -17,6 +17,9 @@ public class Player : MonoBehaviour
     public LayerMask whatIsGround;
 
     public short itemCounter = 0;
+    short itemCounterP = 0;
+
+    public int timer = -1;
 
     void Start()
     {
@@ -26,25 +29,59 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        movementX = Input.GetAxis("Horizontal")*movementSpeed;
+        Movment();
+
+        Itemy();
+    }
+
+    private void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundChack.position, checkRadius, whatIsGround);
+        rg.velocity = new Vector2(movementX, rg.velocity.y);
+    }
+
+    void Itemy()
+    {
+        if (itemCounter > itemCounterP && timer == -1)
+        {
+            itemCounterP = itemCounter;
+            movementSpeed += 3;
+            jumpHigh += 2;
+            timer = 3000;
+        }
+        if (timer == 0)
+        {
+            movementSpeed -= 3;
+            jumpHigh -= 2;
+            timer--;
+        }
+        else if (timer != -1)
+        {
+            timer--;
+        }
+    }
+
+    void Movment()
+    {
+        movementX = Input.GetAxis("Horizontal") * movementSpeed;
         if (isGrounded && Input.GetKey(KeyCode.Space) && rg.velocity.y <= 0.1)
         {
             rg.velocity = Vector2.up * jumpHigh;
         }
-        if(rg.velocity.y >=0.1)
+        if (rg.velocity.y >= 0.1)
             anim.SetBool("Skok", true);
         if (rg.velocity.y < 0)
             anim.SetBool("Skok", false);
-        if(isGrounded)
+        if (isGrounded)
             anim.SetBool("Ground", true);
         else
             anim.SetBool("Ground", false);
         if (movementX > 0)
         {
-            transform.rotation = new Quaternion(transform.rotation.x, 180, transform.rotation.z,transform.rotation.w);
+            transform.rotation = new Quaternion(transform.rotation.x, 180, transform.rotation.z, transform.rotation.w);
             anim.SetBool("Chuze", true);
         }
-        if(movementX<0)
+        if (movementX < 0)
         {
             transform.rotation = new Quaternion(transform.rotation.x, 0, transform.rotation.z, transform.rotation.w);
             anim.SetBool("Chuze", true);
@@ -52,21 +89,15 @@ public class Player : MonoBehaviour
         if (movementX == 0)
         {
             anim.SetBool("Chuze", false);
-            
+
         }
         if (transform.position.x >= border.position.x)
         {
-            transform.position = new Vector2(-(transform.position.x-0.2f), transform.position.y);
+            transform.position = new Vector2(-(transform.position.x - 0.2f), transform.position.y);
         }
         if (transform.position.x <= -border.position.x)
         {
-            transform.position = new Vector2(-(transform.position.x+0.2f), transform.position.y);
+            transform.position = new Vector2(-(transform.position.x + 0.2f), transform.position.y);
         }
-    }
-
-    private void FixedUpdate()
-    {
-        isGrounded = Physics2D.OverlapCircle(groundChack.position, checkRadius, whatIsGround);
-        rg.velocity = new Vector2(movementX, rg.velocity.y);
     }
 }
